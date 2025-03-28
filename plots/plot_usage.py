2a929a1b4486af08299162764610ed8c177b62b8
Aadithyaa <eaadithyaa@gmail.com>
2025-03-28 17:42:45 +0530

    RESULTS

---
+++ plots/plot_usage.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Define the CSV file name
+csv1 = "failure/corepi2.csv"
+csv2 = "failure/corepi3.csv"
+
+# Read the CSV file
+df1 = pd.read_csv(csv1)
+df2 = pd.read_csv(csv2)
+
+# Convert the Timestamp column to datetime format
+df1["Timestamp"] = pd.to_datetime(df1["Timestamp"])
+df2["Timestamp"] = pd.to_datetime(df2["Timestamp"])
+
+df1["smoothed_util"] = df1["CPU_Utilization(%)"].rolling(30).sum() / 30
+df2["smoothed_util"] = df2["CPU_Utilization(%)"].rolling(30).sum() / 30
+
+
+# Plot the data
+plt.figure(figsize=(10, 5))
+plt.plot(df1["Timestamp"], df1["smoothed_util"], label="Node 1", color="b")
+plt.plot(df2["Timestamp"], df2["smoothed_util"], label="Node 2", color="g")
+
+plt.xlabel("Time")
+plt.ylabel("CPU Utilization (%)")
+plt.title("CPU Utilization Over Time")
+plt.legend()
+plt.xticks(rotation=45)
+plt.grid()
+plt.show()
