d812ced326db30248a12a9df84f5b1389d356142
Aadithyaa <eaadithyaa@gmail.com>
2025-03-29 12:33:01 +0530

    New high load plot

--- plots/high_load2/plot_usage.py
+++ plots/high_load2/plot_usage.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 # Define the CSV file name
-csv1 = "corepi2.csv"
-csv2 = "corepi3.csv"
+csv1 = "compute2.csv"
+csv2 = "compute3.csv"
 
 # Read the CSV file
 df1 = pd.read_csv(csv1)
@@ -13,7 +13,7 @@ df2 = pd.read_csv(csv2)
 
 def transform_dataframe(df, start_time, smoothing):
     newdf = df.copy()
-    newdf["timestamp"] = (pd.to_datetime(newdf["Timestamp"]) - pd.to_datetime(start_time)).dt.total_seconds()
+    newdf["timestamp"] = (pd.to_datetime(newdf["Timestamp"]) - pd.to_datetime(start_time)).dt.total_seconds() - 80
     newdf["util"] = newdf["CPU_Utilization(%)"].rolling(smoothing).mean()
     # conv = np.ones(smoothing)*0.9/(smoothing-1)
     # conv[-1] = 0.1
@@ -24,7 +24,7 @@ def transform_dataframe(df, start_time, smoothing):
 
 # Convert the Timestamp column to datetime format
 minTS = min(df1["Timestamp"].min(), df2["Timestamp"].min())
-smoothing = 10
+smoothing = 30
 
 df1 = transform_dataframe(df1, minTS, smoothing)
 df2 = transform_dataframe(df2, minTS, smoothing)
@@ -35,6 +35,7 @@ df2.to_csv("corepi3_transformed.csv", index=False)
 
 # Plot the data
 plt.figure(figsize=(10, 5))
+plt.xlim(0, 95)
 plt.plot(df1["timestamp"], df1["util"], label="Node 1", color="b")
 plt.plot(df2["timestamp"], df2["util"], label="Node 2", color="g")
 
